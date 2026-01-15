4155a2655ad8b57158f25fd0389258666c2b35c8
Mark Takken <mark.e.takken@gmail.com>
2026-01-15 18:09:23 -0500

    playing with scaling

--- kf2vec/models.py
+++ kf2vec/models.py
@@ -87,7 +87,8 @@ class NeuralNetFSW(nn.Module):
         
         # 3. Weight Initialization
         # FC1: Kaiming Normal (accounts for ReLU "killing" half the variance)
-        init.kaiming_normal_(self.fc1.weight, mode='fan_in', nonlinearity='relu')
+        # init.kaiming_normal_(self.fc1.weight, mode='fan_in', nonlinearity='relu')
+        init.xavier_normal_(self.fc1.weight, nonlinearity='relu')
         
         # FC2: Xavier Normal (Output layer is linear/symmetric, so Xavier is safe)
         init.xavier_normal_(self.fc2.weight)
